65604bb63d5704587e041e4988ebd9dfee16bc01
Gabriel Santos <harahujo@gmail.com>
2023-12-14 12:14:33 -0300

    Treino4Linhas

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ titulo.id = 'titulo';
 titulo.innerText = 'Feito com JavaScript';
 document.body.appendChild(titulo);
 
+const titulo2 = document.createElement('h2');
+titulo2.id = 'titulo2';
+titulo2.innerText = 'Segunda Linha feita com Javascript';
+document.body.appendChild(titulo2);
+
+const titulo3 = document.createElement('h3');
+titulo3.id = "titulo3";
+titulo3.innerText =  'Terceira linha feita com Javascript';
+document.body.appendChild(titulo3);
+
+const titulo4 = document.createElement('h4');
+titulo4.id = "titulo4";
+titulo4.innerText = 'Quarta Lnha feita com JavaScript';
+document.body.appendChild(titulo4);
